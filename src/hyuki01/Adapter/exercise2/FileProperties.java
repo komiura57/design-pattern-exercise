9abc5d3c790b3aa984f9f3dbe410667202fda7d8
komiura57 <miura.koichi@gmail.com>
2017-05-05 18:17:02 +0900

    modify from Transfer(pseudo) to Inheritance

--- src/hyuki01/Adapter/exercise2/FileProperties.java
+++ src/hyuki01/Adapter/exercise2/FileProperties.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.Properties;
 
 public class FileProperties extends Properties implements FileIO {
-    private Properties properties = new Properties();
 
     public void readFromFile(String filename) throws IOException {
         File file = new File(filename);
         InputStream in = new FileInputStream(file);
-        properties.load(in);
+        load(in);
     }
 
     public void writeToFile(String filename) throws IOException {
@@ -16,14 +15,14 @@ public class FileProperties extends Properties implements FileIO {
         OutputStream out = new FileOutputStream(file);
 
         String header = "written by FileProperties";
-        properties.store(out,header);
+        store(out,header);
 
     }
     public void setValue(String key, String value) {
-        properties.setProperty(key, value);
+        setProperty(key, value);
     }
 
     public String getValue(String key) {
-        return properties.getProperty(key);
+        return getProperty(key);
     }
 }
